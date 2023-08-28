efc33539ee80308b4fe38635f6a54bd8aed71520
Daniel Ferreira <silva_120000@yahoo.com.br>
2023-08-27 21:30:13 -0300

    criando cadastro de usuario

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService],
+  imports: [PrismaModule],
 })
 export class UserModule {}
